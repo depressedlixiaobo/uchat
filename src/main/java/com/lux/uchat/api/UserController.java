f130d12c747cfa014ed6149ae48eece2ecbe2571
libo <398389303@qq.com>
2019-03-21 00:50:59 +0800

    add upload and save

--- src/main/java/com/lux/uchat/api/UserController.java
+++ src/main/java/com/lux/uchat/api/UserController.java
@@ -5,9 +5,15 @@ import com.lux.uchat.domain.User;
 import com.lux.uchat.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
@@ -16,6 +22,8 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService _userService;
+    @Value("${custom.upload.filePath}")
+    private String _filePath;
 
     @ApiOperation(value="获取用户列表", notes="11")
     @RequestMapping(value = "/getuserlist",method = RequestMethod.GET)
@@ -48,8 +56,16 @@ public class UserController {
     @PostMapping("/api/upload")
     public ApiResult<String> upload(@RequestParam("file") MultipartFile file){
         ApiResult result  = new ApiResult();
-        System.out.println(file.getOriginalFilename());
+        String fileName  = file.getOriginalFilename();
 
+        String savePath = _filePath+fileName;
+        File destFile = new File(savePath);
+        try {
+            file.transferTo(destFile);
+            result.Make(0,"文件上传成功","");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return  result;
     }
